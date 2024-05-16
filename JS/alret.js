332f332054851507db99483a500ce74b847f4290
icpcAbdelrahman <01149455340flks55@gmail.com>
2024-05-16 17:40:04 +0300

    Add files via upload

---
+++ JS/alret.js
@@ -0,0 +1,40 @@
+
+// All Function exist in file.html
+
+// login.html
+
+/*
+
+    function checkInput() {
+     var inputemail = document.getElementById('email').value;     
+     var inputpass = document.getElementById('password').value;     
+        if (inputemail === "") {
+            alert("Email is Empty");
+        }
+        if (inputpass === "") {
+            alert("password is Empty");
+        }
+    }
+
+*/
+
+// Sgin Up
+
+/*
+
+        function checkInput() {
+         var inputusername = document.getElementById('username').value;     
+         var inputemail = document.getElementById('email').value;     
+         var inputpass = document.getElementById('password').value; 
+            if (inputusername === "") {
+                alert("username is Empty");
+            }    
+            if (inputemail === "") {
+                alert("Email is Empty");
+            }
+            if (inputpass === "") {
+                alert("password is Empty");
+            }
+        }
+
+*/
---
+++ JS/change_color.js
@@ -0,0 +1,87 @@
+function changeRed() {
+    var heading = document.getElementById("header");
+    heading.style.backgroundColor = "#b81e1e";
+
+    var heading1 = document.getElementById("button");
+    heading1.style.backgroundColor = "#b81e1e";
+    
+    var heading2 = document.getElementById("btn");
+    heading2.style.backgroundColor = "#b81e1e";
+    
+    var heading3 = document.getElementById("btn2");
+    heading3.style.backgroundColor = "#b81e1e";
+    
+    var heading4 = document.getElementById("btn3");
+    heading4.style.backgroundColor = "#b81e1e";
+    
+    var heading5 = document.getElementById("footer");
+    heading5.style.backgroundColor = "#b81e1e";
+
+}
+
+
+function changeBlue() {
+    var heading = document.getElementById("header");
+    heading.style.backgroundColor = "#232281";
+
+    var heading1 = document.getElementById("button");
+    heading1.style.backgroundColor = "#232281";
+    
+    var heading2 = document.getElementById("btn");
+    heading2.style.backgroundColor = "#232281";
+    
+    var heading3 = document.getElementById("btn2");
+    heading3.style.backgroundColor = "#232281";
+    
+    var heading4 = document.getElementById("btn3");
+    heading4.style.backgroundColor = "#232281";
+    
+    var heading5 = document.getElementById("footer");
+    heading5.style.backgroundColor = "#232281";
+
+}
+
+
+function changeGreen() {
+    var heading = document.getElementById("header");
+    heading.style.backgroundColor = "#138819";
+
+    var heading1 = document.getElementById("button");
+    heading1.style.backgroundColor = "#138819";
+    
+    var heading2 = document.getElementById("btn");
+    heading2.style.backgroundColor = "#138819";
+    
+    var heading3 = document.getElementById("btn2");
+    heading3.style.backgroundColor = "#138819";
+    
+    var heading4 = document.getElementById("btn3");
+    heading4.style.backgroundColor = "#138819";
+    
+    var heading5 = document.getElementById("footer");
+    heading5.style.backgroundColor = "#138819";
+
+}
+
+
+function changeReset() {
+    var heading = document.getElementById("header");
+    heading.style.backgroundColor = "#333";
+
+    var heading1 = document.getElementById("button");
+    heading1.style.backgroundColor = "#333";
+    
+    var heading2 = document.getElementById("btn");
+    heading2.style.backgroundColor = "#333";
+    
+    var heading3 = document.getElementById("btn2");
+    heading3.style.backgroundColor = "#333";
+    
+    var heading4 = document.getElementById("btn3");
+    heading4.style.backgroundColor = "#333";
+    
+    var heading5 = document.getElementById("footer");
+    heading5.style.backgroundColor = "#333";
+
+}
+
